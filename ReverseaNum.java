944d7e23ab3a5d158ed51eeaf99a838a5d00a493
Apsar Shaik <111128105+ApsarShaik06@users.noreply.github.com>
2024-11-07 10:59:25 +0530

    Create ReverseaNum.java

---
+++ ReverseaNum.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Loops {
+    public static void main(String[] args) {
+        // Reverse A number.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        int reverse=0;
+        while(n != 0){
+            int newNum = n%10; // it will give you reminder as n = 123 then reminder is 3
+            reverse = reverse*10 + newNum;
+            n = n/10;
+        }
+        System.out.println("The Reverse number is : "+reverse);
+    }
+}
