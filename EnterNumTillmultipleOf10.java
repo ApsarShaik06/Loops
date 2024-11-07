1239e17556c3f7ca982484e2b846fd9c8e8650dd
Apsar Shaik <111128105+ApsarShaik06@users.noreply.github.com>
2024-11-07 11:13:07 +0530

    Create EnterNumTillmultipleOf10.java

---
+++ EnterNumTillmultipleOf10.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Loops {
+    public static void main(String[] args) {
+        //Keep entering numbers till user enter a multiple of 10.
+        Scanner sc = new Scanner(System.in);
+          do{
+            int num = sc.nextInt();
+            if(num%10 == 0){
+                break;
+            }
+            System.out.println(num);
+          }while(true);
+        System.out.println("I am out of the loop as you entered multiple of 10");
+    }
+}
