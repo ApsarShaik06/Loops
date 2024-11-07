ca7ced49528c53d7677d5eb608a43dcf88302222
Apsar Shaik <111128105+ApsarShaik06@users.noreply.github.com>
2024-11-07 11:38:54 +0530

    Create CheckPrimeOrNot.java

---
+++ CheckPrimeOrNot.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Loops {
+    public static void main(String[] args) {
+        //Display all numbers entered by the user except multiple of 10.
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        boolean isPrime = true;
+        for(int i=2; i<= n/2; i++) {
+            if(n % i == 0){
+               isPrime = false;
+               break;
+            }
+        }
+
+        if(isPrime){
+            System.out.println("Given number is Prime");
+        } else {
+            System.out.println("Given number is not Prime");
+        }
+    }
+}
