e83890b741dc2fcbeaf3b00284f23b449dec438e
Apsar Shaik <111128105+ApsarShaik06@users.noreply.github.com>
2024-11-07 10:23:10 +0530

    Create printNums1to10.java

---
+++ printNums1to10.java
@@ -0,0 +1,10 @@
+public class Loops {
+    public static void main(String[] args) {
+        // Print Numbers from 1 to 10 using while loop
+        int i =1;
+        while(i <= 10){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
